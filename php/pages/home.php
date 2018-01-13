50c725a43878406affa8ce8dbfe7c3a3d49d7e41
Cyril Raffatelli <craffate@student.42.fr>
2018-01-13 20:06:20 +0100

    lolilol

--- php/pages/home.php
+++ php/pages/home.php
@@ -1,4 +1,23 @@
 <div class="homepage">
-<hr />
-	ft_minishop<br />
+	<div id="text">
+		Welcome to ft_minishop<br />
+		The go-to place for your everyday high-tech needs<br />
+		<hr />
+		<div id="showcase">
+			<div class="item">
+				<img src="../www/media/iphone-x.png" alt="iphone-x">
+			</div>
+			<div class="item">
+				<img src="../www/media/oneplus-5t.png" alt="oneplus-5t">
+			</div>
+			<div class="item">
+				<img src="../www/media/lightsaber.png" alt="lightsaber">
+			</div>
+			<div class="item">
+				<img src="../www/media/hhkb.png" alt="hhkb">
+			</div>
+		</div>
+		<hr />
+		Whether it exists or not, we have it...
+	</div>
 </div>
