34c972e0a5aa85852e08e4cf62dab1429e71ad11
Edouard RUCQUOY <erucquoy@e1r2p12.42.fr>
2018-01-13 18:26:36 +0100

    todo and menu

--- php/inc/menu.php
+++ php/inc/menu.php
@@ -1,10 +1,12 @@
 <div id="menu">
     <ul>
+        <li>Our products :<ul>
     <?php 
         foreach ($categories as $category) {
             echo '<li><a href="?p=products&category='. $category['name'] .'">'. $category['name'] .'</a></li>';
         }
     ?>
+        </ul></li>
     <?php if (!$connected) { ?>
     <li><a href="?p=register">Register</a></li>
     <li><a href="?p=login">Login</a></li>
