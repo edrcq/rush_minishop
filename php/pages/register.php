5c0ceeaab113a994d8744eed92ca42daf364b65b
Edouard RUCQUOY <erucquoy@e1r2p9.42.fr>
2018-01-13 14:50:40 +0100

    front login register footer home

--- php/pages/register.php
+++ php/pages/register.php
@@ -2,6 +2,7 @@
     <div class="form">
         Email : <input class="form-input" type="email" name="email" id="email" /><br />
         Password : <input class="form-input" type="password" name="password" id="password" /><br />
-        <button type="button" class="btn" onclick="login" />
+        Repeat password : <input class="form-input" type="password" name="rpassword" id="rpassword" /><br />
+        <button type="button" class="btn" onclick="register" />
     </div>
 </div>
