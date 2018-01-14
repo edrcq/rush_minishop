b5554bfde12ac42b1b2df5a0dd82a01032af6ddf
Edouard RUCQUOY <erucquoy@e1r2p12.42.fr>
2018-01-14 19:20:13 +0100

    delete prod

--- www/ajax/deleteProduct.php
+++ www/ajax/deleteProduct.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['admin']) || $_SESSION['admin'] === false) {
     die();
 }
 if (isset($_POST['id']) && filter_var($_POST['id'], FILTER_VALIDATE_INT))
-    $aff_row = ProductManagerUpdate($_POST['id']);
+    $aff_row = ProductManagerDelete($_POST['id']);
 
 header('Location: ../index.php?p=aproducts');
 die();
