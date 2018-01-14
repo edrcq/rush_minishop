5a2bf7bb72d363b20598894a2cbf2110b2ace734
Edouard RUCQUOY <erucquoy@e1r2p12.42.fr>
2018-01-14 19:38:11 +0100

    aord

---
+++ www/ajax/deleteOrder.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();
+
+require_once(__DIR__ . '/../../php/init.php');
+
+if (!isset($_SESSION['connected'])) {
+    header('Location: ../index.php?p=home');
+    die();
+}
+if (!isset($_SESSION['admin']) || $_SESSION['admin'] === false) {
+    header('Location: ../index.php?p=home');
+    die();
+}
+if (isset($_POST['id']) && filter_var($_POST['id'], FILTER_VALIDATE_INT))
+    $aff_row = DeleteManagerDelete($_POST['id']);
+
+header('Location: ../index.php?p=aord');
+die();
+
+?>
