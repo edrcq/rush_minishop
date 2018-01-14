49c6640522813347580582cfce8cc28b2f23d925
Edouard RUCQUOY <erucquoy@e1r2p12.42.fr>
2018-01-14 21:59:47 +0100

    ausers

--- www/ajax/addProduct.php
+++ www/ajax/addProduct.php
@@ -17,18 +17,19 @@ $prod = [
 ];
 
 if (isset($_POST['name']) && isset($_POST['category']) && isset($_POST['color']) && isset($_POST['description']) && isset($_POST['stcok']) && isset($_POST['price'])) {
+    echo 'ok';
     $prod['name'] = $_POST['name'];
     $prod['category'] = $_POST['category'];
     $prod['color'] = $_POST['color'];
     $prod['description'] = $_POST['description'];
-    $prod['stock'] = $_POST['stock'];
-    $prod['price'] = $_POST['price'];
+    $prod['stock'] = intval($_POST['stock']);
+    $prod['price'] = floatval($_POST['price']);
     $pid = ProductManagerAdd($prod);
 }
 var_dump($_POST);
 var_dump($prod);
 var_dump($pid);
 
-header('Location: ../index.php?p=aproducts');
+//header('Location: ../index.php?p=aproducts');
 die();
 ?>
