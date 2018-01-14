75c95dfe113d246af41a26226e4f19136ac612d0
Cyril Raffatelli <craffate@student.42.fr>
2018-01-14 18:00:16 +0100

    hash

--- www/ajax/updateAcc.php
+++ www/ajax/updateAcc.php
@@ -9,7 +9,7 @@
 		if ($user['email'] !== $_POST['email'])
 			$user['email'] = $_POST['email'];
 		if (($user['password'] !== $_POST['password']) && $_POST['password'])
-			$user['password'] = $_POST['password'];
+			$user['password'] = password_hash($_POST['password'], "sha512");
 		UserManagerUpdate($user);
 		print("Updated");
 		header('Location: ../index.php?p=home');
