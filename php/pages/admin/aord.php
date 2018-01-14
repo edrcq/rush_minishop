b7de080698474d558ec79d1a423812158fb1065f
Edouard RUCQUOY <erucquoy@e1r2p12.42.fr>
2018-01-14 19:35:59 +0100

    aord

--- php/pages/admin/aord.php
+++ php/pages/admin/aord.php
@@ -14,16 +14,16 @@
 		<?php
 			foreach ($p as $idx) {
 				echo '<tr>';
-		?>
-			<td><input type="text" name="id" form="product" placeholder="<?php echo $idx['id']; ?>"</td>
-			<td><input type="text" name="id" form="product" placeholder="<?php echo $idx['list']; ?>"</td>
-			<td><input type="text" name="id" form="product" placeholder="<?php echo $idx['total']; ?>"</td>
-			<td><input type="text" name="id" form="product" placeholder="<?php echo $idx['nb']; ?>"</td>
-			<td><input type="text" name="id" form="product" placeholder="<?php echo $idx['order_date']; ?>"</td>
-			<td><input type="text" name="id" form="product" placeholder="<?php echo $idx['jsondata']; ?>"</td>
-			<td><input type="text" name="id" form="product" placeholder="<?php echo $idx['status']; ?>"</td>
-			<td><input type="submit" value="Update" /></td>
-			<td><input type="submit" value="Delete" /></td>
+		?><form action="ajax/updateOrder.php" method="post">
+			<td><input type="text" name="id" value="<?php echo $idx['id']; ?>"/></td>
+			<td><input type="text" name="list"  value="<?php echo htmlspecialchars($idx['list']); ?>"/></td>
+			<td><input type="text" name="total" value="<?php echo $idx['total']; ?>"/></td>
+			<td><input type="text" name="nb"  value="<?php echo $idx['nb']; ?>"/></td>
+			<td><input type="text" name="order_date" value="<?php echo $idx['order_date']; ?>"/></td>
+			<td><input type="text" name="jsondata"  value="<?php echo $idx['jsondata']; ?>"/></td>
+			<td><input type="text" name="status"  value="<?php echo $idx['status']; ?>"/></td>
+			<td><input type="submit" value="Update" /></td></form>
+			<td><form action="ajax/deleteOrder.php" method="POST"><input type="hidden" name="id" value="<?php echo $idx['id']; ?>"/><input type="submit" value="Delete" /></form></td>
 		<?php echo '<tr>';} ?>
 	</table>
 </div>
