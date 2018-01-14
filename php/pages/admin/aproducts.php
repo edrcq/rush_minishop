70814b1076df0cd67493b8d1887dd5b83e5a937a
Edouard RUCQUOY <erucquoy@e1r2p12.42.fr>
2018-01-14 17:27:03 +0100

    aprod

--- php/pages/admin/aproducts.php
+++ php/pages/admin/aproducts.php
@@ -18,14 +18,14 @@
 				echo '<tr>';
 		?><form action="ajax/updateProduct.php" method="post">
 			<td><img src="http://chestnutglobalpartners.in/wp-content/uploads/2017/06/placeholder.png" /><!--<?php echo $idx['img']; ?>--></td>
-			<td><input type="text" name="id" form="product" placeholder="<?php echo $idx['id']; ?>"</td>
-			<td><input type="text" name="name" form="product" placeholder="<?php echo $idx['name']; ?>"</td>
-			<td><input type="text" name="category" form="product" placeholder="<?php echo $idx['category']; ?>"</td>
-			<td><input type="text" name="color" form="product" placeholder="<?php echo $idx['color']; ?>"</td>
-			<td><input type="text" name="description" form="product" placeholder="<?php echo $idx['description']; ?>"</td>
-			<td><input type="text" name="stock" form="product" placeholder="<?php echo $idx['stock']; ?>"</td>
-			<td><input type="text" name="jsondata" form="product" placeholder="<?php echo $idx['jsondata']; ?>"</td>
-			<td><input type="text" name="price" form="product" placeholder="<?php echo $idx['price']; ?>"</td>
+			<td><input type="text" name="id" form="product" value="<?php echo $idx['id']; ?>"</td>
+			<td><input type="text" name="name" form="product" value="<?php echo $idx['name']; ?>"</td>
+			<td><input type="text" name="category" form="product" value="<?php echo $idx['category']; ?>"</td>
+			<td><input type="text" name="color" form="product" value="<?php echo $idx['color']; ?>"</td>
+			<td><input type="text" name="description" form="product" value="<?php echo $idx['description']; ?>"</td>
+			<td><input type="text" name="stock" form="product" value="<?php echo $idx['stock']; ?>"</td>
+			<td><input type="text" name="jsondata" form="product" value="<?php echo $idx['jsondata']; ?>"</td>
+			<td><input type="text" name="price" form="product" value="<?php echo $idx['price']; ?>"</td>
 			<td><input type="submit" name="update" id="update_<?php echo $idx['id']; ?>" value="Update" onclick="updateProduct(<?php echo $idx['id']; ?>)" /></td>
 			<td><input type="submit" name="delete" id="delete_<?php echo $idx['id']; ?>" value="Delete" onclick="deleteProduct(<?php echo $idx['id']; ?>)" /></td></form>
 		<?php echo '<tr>';} ?>
